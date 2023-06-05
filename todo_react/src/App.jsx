6cf8984ac4a91fba23cce3a7e16b3d6ec04abb03
Khant Zaw Hein <khantzawhein14@gmail.com>
2023-06-05 22:37:30 +0800

    added navbar with react-router

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -1,27 +1,27 @@
 import Todo from './components/Todo/Todo'
-// import LoginPage from './components/Login/LoginPage';
-// import { Route, Routes, BrowserRouter } from 'react-router-dom';
-// import NotFound from './components/Helpers/NotFound';
-// import Home from './components/Home/Home';
-// import Crypto from './components/Crypto/Crypto';
-// import Navbar from './components/Navbar/Navbar';
+import LoginPage from './components/Login/LoginPage';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import NotFound from './components/Helpers/NotFound';
+import Home from './components/Home/Home';
+import Crypto from './components/Crypto/Crypto';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
 
   return (
     // <LoginPage/>
-    <Todo/>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Navbar/>} >
-    //       <Route index element={<Home />} />
-    //       <Route path="Todo" element={<Todo />} />
-    //       <Route path="Crypto" element={<Crypto />} />
-    //       <Route path="Login" element={<LoginPage />} />
-    //       <Route path="*" element={<NotFound />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
+    // <Todo/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar/>} >
+          <Route index element={<Home />} />
+          <Route path="Todo" element={<Todo />} />
+          <Route path="Crypto" element={<Crypto />} />
+          <Route path="Login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
   );
 }
